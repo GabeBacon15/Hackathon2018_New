b2d9ff532529dfc9eede926367282c1ff6401fc0
jvanhyn <jvanhyn@roadrunner.com>
2018-11-30 22:46:34 -0800

    Create GenerateNext.java

---
+++ Music/src/Driving/GenerateNext.java
@@ -0,0 +1,29 @@
+package Driving;
+
+public class GenerateNext {
+
+	public int chord(int prevChord) {
+		double randomNum = Math.random();
+		int nextChord;
+
+		if (prevChord == 1) {
+			
+
+		}
+		if (prevChord == 2) {
+
+		}
+		if (prevChord == 4) {
+
+		}
+		if (prevChord == 5) {
+
+		}
+		if (prevChord == 6) {
+
+		}
+
+		return nextChord;
+	}
+
+}
